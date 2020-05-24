1dbbbd7f09e1f23ec0d0f9b04c301fe977adf03c
Olga Sleptsova <osleptsova2gmail.com>
2020-05-24 20:36:38 +0300

    first file

--- task.py
+++ task.py
@@ -1,10 +1,10 @@
-class Аnimals:
+class Animals:
     food = 'stop eating'
     voice = 'stop speak'
     color = 'white'
     size = 'small'
     age = 0
-    weight = 0
+    weight = {}
     name = 'no name'
 
     def start_eat(self):
@@ -18,18 +18,20 @@ class Аnimals:
 
     def stop_publish_a_voice(self):
         self.voice = 'stop speak'
-        pass
 
 
-class Birds(Аnimals):
-    eggs = 'not lay eggs'
+pass
 
-    def __init__(self, food, voice, color, weight, name):
-        self.food = 'stop eating'
-        self.voice = 'stop speak'
-        self.color = 'white'
-        self.weight = 0
-        self.name = 'no name'
+
+class Birds(Animals):
+
+    def __init__(self, food, voice, color, weight, name, eggs):
+        self.food = food
+        self.voice = voice
+        self.color = color
+        self.weight = weight
+        self.name = name
+        self.eggs = eggs
 
     def lay_eggs(self):
         self.eggs = 'lay eggs'
@@ -37,18 +39,22 @@ class Birds(Аnimals):
     def stop_lay_eggs(self):
         self.eggs = 'not lay eggs'
 
-    pass
+    def add(self):
+        Animals.weight.update({self.name: self.weight})
 
 
-class Сows_and_goats(Аnimals):
-    milk = 'no'
+pass
 
-    def __init__(self, food, voice, color, weight, name):
-        self.food = 'stop eating'
-        self.voice = 'stop speak'
-        self.color = 'white'
-        self.weight = 0
-        self.name = 'no name'
+
+class СowsAndGoats(Animals):
+
+    def __init__(self, food, voice, color, weight, name, milk):
+        self.food = food
+        self.voice = voice
+        self.color = color
+        self.weight = weight
+        self.name = name
+        self.milk = milk
 
     def to_give_milk(self):
         self.milk = 'yes'
@@ -56,18 +62,21 @@ class Сows_and_goats(Аnimals):
     def do_not_give_milk(self):
         self.milk = 'no'
 
+    def add(self):
+        Animals.weight.update({self.name: self.weight})
+
     pass
 
 
-class Sheeps(Аnimals):
-    wool = 'no'
+class Sheeps(Animals):
 
-    def __init__(self, food, voice, color, weight, name):
-        self.food = 'stop eating'
-        self.voice = 'stop speak'
-        self.color = 'white'
-        self.weight = 0
-        self.name = 'no name'
+    def __init__(self, food, voice, color, weight, name, wool):
+        self.food = food
+        self.voice = voice
+        self.color = color
+        self.weight = weight
+        self.name = name
+        self.wool = wool
 
     def cut_hair(self):
         self.wool = 'yes'
@@ -75,66 +84,66 @@ class Sheeps(Аnimals):
     def do_not_cut_hair(self):
         self.wool = 'no'
 
+    def add(self):
+        Animals.weight.update({self.name: self.weight})
+
     pass
 
 
-goose1 = Birds()
+goose1 = Birds('stop eating', 'stop speak', 'gray', 10, 'гусь Серый', 'not lay eggs')
 goose1.start_eat()
 goose1.lay_eggs()
-goose1.name = 'Серый'
-goose1.weight = 10
+goose1.add()
 
-goose2 = Birds()
+goose2 = Birds('stop eating', 'stop speak', 'white', 20, 'гусь Белый', 'not lay eggs')
 goose2.start_eat()
 goose2.lay_eggs()
-goose1.name = 'Белый'
-goose1.weight = 20
+goose2.add()
 
-cow = Сows_and_goats()
+cow = СowsAndGoats('stop eating', 'stop speak', 'brown', 50, ' корова Манька', 'no')
 cow.start_eat()
 cow.to_give_milk()
-cow.name = 'Манька'
-cow.weight = 50
+cow.add()
 
-sheep1 = Sheeps()
+sheep1 = Sheeps('stop eating', 'stop speak', 'white', 15, 'овца Барашек', 'no')
 sheep1.start_eat()
 sheep1.cut_hair()
-sheep1.name = 'Барашек'
-sheep1.weight = 15
+sheep1.add()
 
-sheep2 = Sheeps()
+sheep2 = Sheeps('stop eating', 'stop speak', 'white', 14, 'овца Кудрявый', 'no')
 sheep2.start_eat()
 sheep2.cut_hair()
-sheep2.name = 'Кудрявый'
-sheep2.weight = 14
+sheep2.add()
 
-chicken1 = Birds()
+chicken1 = Birds('stop eating', 'stop speak', 'gray', 4, 'курица Ко-ко', 'not lay eggs')
 chicken1.start_eat()
 chicken1.lay_eggs()
-chicken1.name = 'Ко-ко'
-chicken1.weight = 4
+chicken1.add()
 
-chicken2 = Birds()
+chicken2 = Birds('stop eating', 'stop speak', 'white', 5, 'курица Кукареку', 'not lay eggs')
 chicken2.start_eat()
 chicken2.lay_eggs()
-chicken2.name = 'Кукареку'
-chicken2.weight = 5
+chicken2.add()
 
-goat1 = Сows_and_goats()
+goat1 = СowsAndGoats('stop eating', 'stop speak', 'gray', 10, 'коза Рога', 'no')
 goat1.start_eat()
 goat1.to_give_milk()
-goat1.name = 'Рога'
-goat1.weight = 10
+goat1.add()
 
-goat2 = Сows_and_goats()
+goat2 = СowsAndGoats('stop eating', 'stop speak', 'gray', 11, 'коза Копыта', 'no')
 goat2.start_eat()
 goat2.to_give_milk()
-goat2.name = 'Копыта'
-goat2.weight = 11
+goat2.add()
 
-duck = Birds()
+duck = Birds('stop eating', 'stop speak', 'white', 6, 'утка Кряква', 'not lay eggs')
 duck.start_eat()
 duck.lay_eggs()
-duck.name = 'Кряква'
-duck.weight = 6
-print(duck.weight)
+duck.add()
+
+x = max(Animals.weight.values())
+
+for z, y in Animals.weight.items():
+    if y == x:
+        print(f'Самое тяжелое животное -{z}')
+
+print(f'Вес всех животных - {sum(Animals.weight.values())}')
